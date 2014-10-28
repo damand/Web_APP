d42cd161082adb9c33a822df23dc286764182f5d
unknown <damandhillon23@gmail.com>
2014-10-27 17:29:56 -0700

    Basic Setup 1st Commit

---
+++ AmazingTodo/AmazingTodo/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace AmazingTodo.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ AmazingTodo/AmazingTodo/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace AmazingTodo.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
